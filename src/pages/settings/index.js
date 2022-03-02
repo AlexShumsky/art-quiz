29ca1a6436e5f80ce05deecaf5f3b02cec77f5cf
AlexShumsky <sanya.shumskiy.95@mail.ru>
2022-03-03 00:40:06 +0300

    feat: add settings page

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -12,5 +12,37 @@ class SettingsPage {
     containerInner.innerHTML = SettingsHtml;
     return containerInner.content.firstChild;
   }
+
+  settingsManage() {
+    function customizeBars(el) {
+      const bar = el.nextElementSibling;
+      bar.style.width = `${
+        el.max === '1' ? el.value * 100 : el.value * 4 - 20
+      }%`;
+    }
+    function customizeThumbs(el) {
+      const thumb = el.nextElementSibling.nextElementSibling;
+      thumb.style.left = `${
+        el.max === '1' ? el.value * 100 : el.value * 4 - 20
+      }%`;
+    }
+    function customizeInput() {
+      customizeBars(this);
+      customizeThumbs(this);
+    }
+
+    function drawCustomInputs() {
+      const settingsSection = document.querySelector('.settings__section');
+
+      settingsSection.addEventListener('mouseover', function (event) {
+        const targetEl = event.target;
+        if (targetEl.nodeName === 'INPUT' && targetEl.type === 'range') {
+          targetEl.addEventListener('input', customizeInput);
+        }
+      });
+    }
+
+    drawCustomInputs();
+  }
 }
 export default SettingsPage;
